e0e2299e365d771d3c0d94e4a389bed6b1231838
Subhangi <subhangikasture@gmail.com>
2024-12-27 14:05:27 +0530

    added a new home compenent and added home component in the root component search and button are placed in home component

--- house-buiseness-app/src/app/app.component.ts
+++ house-buiseness-app/src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {HomeComponent} from './home/home.component'
 @Component({
   selector: 'app-root',
-  imports:[],
+  imports: [HomeComponent],
   template:`
+  <main>
   <h1> Hello world!! </h1>
+  <section class="content">
+    <app-home></app-home>
+  </section>
+</main>
+
   `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'house-buiseness-app';
+  title = 'homes';
 }
