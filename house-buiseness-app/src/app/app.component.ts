9f3bdaa5dbf6cdc59f377ee4a6c3202e8e0574ff
Subhangi <subhangikasture@gmail.com>
2024-12-28 17:14:12 +0530

    house loaction component is imported inside home component

--- house-buiseness-app/src/app/app.component.ts
+++ house-buiseness-app/src/app/app.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from './home/home.component'
-import {HousingLocationComponent} from './housing-location/housing-location.component'
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, HousingLocationComponent],
+  imports: [HomeComponent],
   template:`
   <main>
   <h1> Hello world!! </h1>
   <section class="content">
     <app-home></app-home>
-   
-  </section>
-  <section class="content">
-  <app-housing-location></app-housing-location>
   </section>
 </main>
 
--- house-buiseness-app/src/app/home/home.component.ts
+++ house-buiseness-app/src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {HousingLocationComponent} from '../housing-location/housing-location.component'
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HousingLocationComponent],
   template:`<section>
   <form>
+  <section class="content">
+        <app-housing-location></app-housing-location>
+      </section>
       <input type="text" placeholder="Filter by city"/>
       <button class="primary" type="button">Search</button>
+    
   </form>
 </section>`,
   styleUrl: './home.component.css'
