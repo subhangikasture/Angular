539137a4f89b4e39c436b5e0bce8f2c6e18328b6
Subhangi <subhangikasture@gmail.com>
2024-12-28 17:50:27 +0530

    add housingLocation interface and create instance in home component

--- house-buiseness-app/src/app/home/home.component.ts
+++ house-buiseness-app/src/app/home/home.component.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
-import {HousingLocationComponent} from '../housing-location/housing-location.component'
-
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import {HousingLocation} from '../housing-location';
 @Component({
   selector: 'app-home',
   imports: [HousingLocationComponent],
-  template:`<section>
+  template:`
+  <section>
   <form>
-  <section class="content">
-        <app-housing-location></app-housing-location>
-      </section>
       <input type="text" placeholder="Filter by city"/>
       <button class="primary" type="button">Search</button>
-    
   </form>
-</section>`,
+</section>
+<section class="content">
+        <app-housing-location></app-housing-location>
+      </section>
+
+`,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocation = {
+    id: 99999,
+    name: "Subhangi",
+    city: "Bnaglore",
+    state: "Karnataka",
+    wifi: true,
+    availableUnits: 8,
+    laundry: true,
+    photo: `${this.baseUrl}/example-house.jpg`
+  };
 }
