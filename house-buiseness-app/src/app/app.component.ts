a574b4ecd9e2826fc9dffdbe99a33753e48ead56
Subhangi <subhangikasture@gmail.com>
2024-12-31 14:37:55 +0530

    Uisng ngFor display housing location in grid format

--- house-buiseness-app/src/app/app.component.ts
+++ house-buiseness-app/src/app/app.component.ts
@@ -1,31 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HomeComponent} from './home/home.component'
-import { HousingLocationComponent } from "./housing-location/housing-location.component";
-import { HousingLocation } from './housing-location';
+import {HomeComponent} from './home/home.component';
+
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, HousingLocationComponent],
+  imports: [HomeComponent],
   template:`
   <main>
   <h1> Hello world!! </h1>
   <section class="content">
     <app-home></app-home>
   </section>
-  <section class="content">
-    <app-housing-location [housingLocation_]="housingLocation_appComponnet"></app-housing-location>
-  </section>
-</main>
-
+  </main>
   `,
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'homes';
-  housingLocation_appComponnet: HousingLocation=
-  {
-    name:"Kartik",
-    id:123,
-    city: "Kalkatta"
-  }
 }
--- house-buiseness-app/src/app/home/home.component.ts
+++ house-buiseness-app/src/app/home/home.component.ts
@@ -1,33 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import {HousingLocationComponent} from '../housing-location/housing-location.component';
 import {HousingLocation} from '../housing-location';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-home',
-  imports: [HousingLocationComponent],
+  imports: [ CommonModule, HousingLocationComponent],
   template:`
-  <section>
-  <form>
-      <input type="text" placeholder="Filter by city"/>
-      <button class="primary" type="button">Search</button>
-  </form>
-</section>
-<section class="content">
-        <app-housing-location [housingLocation_]="housingLocation_homeComponent"></app-housing-location>
-      </section>
-
+    <section>
+    <form>
+        <input type="text" placeholder="Filter by city"/>
+        <button class="primary" type="button">Search</button>
+    </form>
+    </section>
+    <section class="content">
+    <div class="grid">
+        <app-housing-location *ngFor="let location of housingLocationList" [housingLocation_]="location"></app-housing-location>
+    </div>
+    </section>
 `,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  housingLocation_homeComponent: HousingLocation = {
-    id: 99999,
-    name: "Subhangi",
-    city: "Bnaglore",
+
+  housingLocationList: HousingLocation[] = [
+    {
+    id: 99991,
+    name: "Arya stark",
+    city: "North",
     state: "Karnataka",
     wifi: true,
     availableUnits: 8,
     laundry: true,
     photo: `${this.baseUrl}/example-house.jpg`
-  };
+   },
+   {
+    id: 99992,
+    name: "Sansa stark",
+    city: "North",
+    state: "Karnataka",
+    wifi: true,
+    availableUnits: 8,
+    laundry: true,
+    photo: `${this.baseUrl}/example-house.jpg`
+   },
+   {
+    id: 99993,
+    name: "Jon snow",
+    city: "Winterfell",
+    state: "Karnataka",
+    wifi: true,
+    availableUnits: 8,
+    laundry: true,
+    photo: `${this.baseUrl}/example-house.jpg`
+   },
+   {
+    id: 99994,
+    name: "Drangon mother",
+    city: "7 kingdom",
+    state: "Karnataka",
+    wifi: true,
+    availableUnits: 8,
+    laundry: true,
+    photo: `${this.baseUrl}/example-house.jpg`
+   },
+];
 }
