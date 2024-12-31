636a0cf72262770c0ab6607cce0bf7bb8b918a3c
Subhangi <subhangikasture@gmail.com>
2024-12-31 16:13:10 +0530

    enabling the routing

--- house-buiseness-app/src/app/app.component.ts
+++ house-buiseness-app/src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
-import {HomeComponent} from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [ RouterModule],
   template:`
-  <main>
-  <h1> Hello world!! </h1>
-  <section class="content">
-    <app-home></app-home>
-  </section>
-  </main>
+   <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
   `,
   styleUrl: './app.component.css'
 })
