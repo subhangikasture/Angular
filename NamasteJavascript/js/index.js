d0deca5de6246c723ad03601e120d21e7264c15c
Subhangi <subhangikasture@gmail.com>
2025-01-12 18:04:28 +0530

    await wait for promise to resolve before going to next line

--- NamasteJavascript/js/index.js
+++ NamasteJavascript/js/index.js
@@ -1,6 +1,7 @@
 console.log("Hello World");
 
 // async function always returns promise , if you don't return promise then it will wrap the return value in promise
+/*
 async function getData() {
     return "Namaste Javascript";
 }
@@ -8,17 +9,18 @@ const dataPromise = getData();
 console.log(dataPromise); // Promise {<fulfilled>: "Namaste Javascript"}
 dataPromise.then (data => console.log(data)); // Namaste Javascript
 
+
+
+ function getPromiseData() {
+    return  p;
+}*/
 //Create a promise and resolve
 const p = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("Resolved promise data");
-    }, 1000);
+    }, 10000);
 });
-
- function getPromiseData() {
-    return  p;
-}
-
+/*
 const dataPromise2 = getPromiseData();
 
 dataPromise2.then (data => console.log(data)); // Promise {<fulfilled>: "Resolved promise data"}
@@ -40,4 +42,27 @@ function resolveAfter10Seconds() {
   console.log("I am called before promise is resolved");
 }
 
-resolveAfter10Seconds();
+resolveAfter10Seconds();*/
+
+// Let's see how to handle promise using async await with setTimeout of 1000
+
+async function getData2(){
+    const val = await p;
+    console.log("1 Inside getData2 function after promise ");
+    console.log("2 Lets print p for promise "+ val); //ideally 48 line number should wait for 2 secs and then print this 
+    console.log("3 Inside getData2 function after promise ");
+    console.log("4 Lets print p for promise "+val);
+
+}
+
+//getData2();
+
+//let me do this again without using async await to understand the beauty if s=async await
+function testWithoutAwait(){
+    p.then ((res)=>console.log("1 Inside testWithoutAwait function after promise "+res));
+    console.log("2 Inside testWithoutAwait function after promise ");
+}
+testWithoutAwait();
+/*o/p
+2 Inside testWithoutAwait function after promise 
+index.js:62 1 Inside testWithoutAwait function after promise Resolved promise data */
