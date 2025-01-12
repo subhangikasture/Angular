951f9572a6a3c5ca9890473711edfa2354b9c071
Subhangi <subhangikasture@gmail.com>
2025-01-12 16:52:59 +0530

    async await promised

--- NamasteJavascript/js/index.js
+++ NamasteJavascript/js/index.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+// async function always returns promise , if you don't return promise then it will wrap the return value in promise
+async function getData() {
+
+    return "Namaste Javascript";
+}
+ 
+const dataPromise = getData();
+console.log(dataPromise); // Promise {<fulfilled>: "Namaste Javascript"}
+
+dataPromise.then (data => console.log(data)); // Namaste Javascript
+
