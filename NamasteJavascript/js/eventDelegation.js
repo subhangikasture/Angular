dafcca77402f1869625b2e8bd31f256c19bb4b76
Subhangi <subhangikasture@gmail.com>
2025-01-17 18:33:58 +0530

    Event delegation examples

--- NamasteJavascript/js/eventDelegation.js
+++ NamasteJavascript/js/eventDelegation.js
@@ -1,5 +1,17 @@
 document.querySelector("#test")
 .addEventListener("click",(e)=>{
     console.log("Category clicked", e.target.id);
-    window.location.href ="/"+ e.target.id;
+    if(e.target.tagName == "LI")
+    {
+     window.location.href ="/"+ e.target.id;
+    }
+})
+
+document.querySelector('#fullname')
+.addEventListener('input',(e)=>{
+    console.log("Fullname input changed");
+    if(e.target.dataset.uppercase != undefined)
+    {
+        e.target.value = e.target.value.toUpperCase();
+    }
 })
