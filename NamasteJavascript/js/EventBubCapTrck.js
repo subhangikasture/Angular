9fc8af2220a7a94a9be4e90fe2aa63104c6fac22
Subhangi <subhangikasture@gmail.com>
2025-01-17 15:48:08 +0530

    Example of event capturing

--- NamasteJavascript/js/EventBubCapTrck.js
+++ NamasteJavascript/js/EventBubCapTrck.js
@@ -1,14 +1,14 @@
 document.querySelector("#grandParent")
 .addEventListener('click',()=>{
     console.log("Grandparent clicked");
-})
+}, true)// true is for capturing
 
 document.querySelector("#parent")
 .addEventListener('click',()=>{
     console.log("Parent clicked");
-})  
+}, true)  
 
 document.querySelector("#child")
 .addEventListener('click',() => {
     console.log("Child clicked")
-})
+}, true)
